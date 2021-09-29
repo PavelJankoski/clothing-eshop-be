99898b2bbf31a245205dbea2e30a454d9095bc25
Pavel Jankoski <pavel.jankoski@stage.alosys.it>
2021-09-29 16:48:05 +0200

    initial commit, project setup

---
+++ shared-kernel/src/main/java/mk/ukim/finki/sharedkernel/ServletInitializer.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.sharedkernel;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SharedKernelApplication.class);
+    }
+
+}
---
+++ user-management/src/main/java/mk/ukim/finki/usermanagement/ServletInitializer.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.usermanagement;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UserManagementApplication.class);
+    }
+
+}
